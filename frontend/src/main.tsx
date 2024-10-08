34dc7d82be32ae28569715ef9c39776b3114e49a
liuleora@gmail.com <liuleora@gmail.com>
2024-10-08 16:41:23 +0900

    feat: サンプルファイルを削除し、空ページの作成とテスト用の最小構成に変更

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const App = () => {
+    return (
+        <div>
+            <h1>VocaRise</h1>
+        </div>
+    );
+};
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
